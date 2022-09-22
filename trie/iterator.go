d4dcb8dca0af013401249d34bb20e6877d48186a
niuxiaojie81 <85773309@qq.com>
2022-09-22 11:45:02 +0800

    Simple rollback trie

--- trie/iterator.go
+++ trie/iterator.go
@@ -182,13 +182,15 @@ func (it *nodeIterator) LeafBlob() []byte {
 func (it *nodeIterator) LeafProof() [][]byte {
 	if len(it.stack) > 0 {
 		if _, ok := it.stack[len(it.stack)-1].node.(valueNode); ok {
-			hasher := newHasher(false)
+			hasher := newHasher()
 			defer returnHasherToPool(hasher)
+
 			proofs := make([][]byte, 0, len(it.stack))
 
 			for i, item := range it.stack[:len(it.stack)-1] {
 				// Gather nodes that end up as hash nodes (or the root)
-				node, hashed := hasher.proofHash(item.node)
+				node, _, _ := hasher.hashChildren(item.node)
+				hashed, _ := hasher.store(node, false)
 				if _, ok := hashed.(hashNode); ok || i == 0 {
 					enc, _ := rlp.EncodeToBytes(node)
 					proofs = append(proofs, enc)
--- trie/secure_trie.go
+++ trie/secure_trie.go
@@ -193,7 +193,7 @@ func (t *SecureTrie) NodeIterator(start []byte) NodeIterator {
 // The caller must not hold onto the return value because it will become
 // invalid on the next call to hashKey or secKey.
 func (t *SecureTrie) hashKey(key []byte) []byte {
-	h := newHasher(false)
+	h := newHasher()
 	h.sha.Reset()
 	h.sha.Write(key)
 	h.sha.Read(t.hashKeyBuf[:])
